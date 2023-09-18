a6b7bbb666d6b8c6c9e82f83f4146a40517e59f8
delano <delano@cpan.org>
2023-09-18 11:52:48 -0700

    Removed app with generic name requests
    Signed-off-by: delano <delano@cpan.org>

--- afb/requests/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# some_app/views.py
-from django.views.generic import TemplateView
-
-
-class AboutView(TemplateView):
-    template_name = "about.html"
