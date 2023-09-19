3c587d243dba691386c8da85ac0d8a12877c70a4
delano <delano@cpan.org>
2023-09-19 14:14:13 -0700

    Added static asset settings
    Signed-off-by: delano <delano@cpan.org>

--- afb/afb/settings.py
+++ afb/afb/settings.py
@@ -138,7 +138,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# For prod:
+# https://docs.djangoproject.com/en/4.2/howto/static-files/deployment/#staticfiles-from-cdn
+# https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-STORAGES
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "theme" / "static",
+    BASE_DIR / "afbcore" / "public",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
