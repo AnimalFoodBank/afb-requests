bf06fac3c915d4d6bf0057f52232fbd2b5509842
delano <delano@cpan.org>
2023-09-18 17:12:55 -0700

    Added unfold admin templates
    Signed-off-by: delano <delano@cpan.org>

--- afb/afb/settings.py
+++ afb/afb/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 #
 
 INSTALLED_APPS = [
+    'unfold',  # https://github.com/unfoldadmin/django-unfold
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
