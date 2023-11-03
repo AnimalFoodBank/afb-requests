2ea4afb8bed3557542b8521bc0f70865454a9fb6
delano <delano@cpan.org>
2023-11-03 10:26:54 -0700

    Fix API authentication URL.
    Signed-off-by: delano <delano@cpan.org>

--- apps/api/afb/urls.py
+++ apps/api/afb/urls.py
@@ -27,7 +27,7 @@ router.register(r"users", users.UserViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-    # path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),
     # path("login/", views.MyLoginView.as_view(), name="login"),
     # Part of django-tailwind
     # See https://django-tailwind.readthedocs.io/en/latest/installation.html#configuration
