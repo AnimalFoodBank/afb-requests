048d641ca8130fcdcfc7a7bcb1db054e3b749fdd
delano <delano@cpan.org>
2023-11-03 10:34:12 -0700

    Add current_user endpoint to UserViewSet
    Signed-off-by: delano <delano@cpan.org>

--- apps/api/afbcore/views/users.py
+++ apps/api/afbcore/views/users.py
@@ -4,6 +4,8 @@ from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from afbcore.serializers import UserSerializer
 
 User = get_user_model()
@@ -22,3 +24,11 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False, methods=["get"])
+    def current_user(self, request):
+        serializer = self.get_serializer(request.user)
+        # import ipdb
+
+        # ipdb.set_trace()
+        return Response(serializer.data)
