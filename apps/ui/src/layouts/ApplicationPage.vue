c0d02031e8f1d00dcb8db6aeb9208fb4d2f198a1
delano <delano@cpan.org>
2023-11-21 16:46:06 -0800

    Fix bug in login functionality
    Signed-off-by: delano <delano@cpan.org>

--- apps/ui/src/layouts/ApplicationPage.vue
+++ apps/ui/src/layouts/ApplicationPage.vue
@@ -1,3 +1,199 @@
+<!-- NOTE: When setup is missing, the router/index.ts shows a warning that ApplicationPage isn't a module -->
+<script setup lang="ts">
+import defaultAvatar from '@/assets/img/Cat-Heart-680x800-1.png';
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline';
+import axios from 'axios';
+import { computed, defineComponent, h, onMounted, reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '../stores/auth';
+
+// See: https://router.vuejs.org/api/interfaces/Router.html
+const router = useRouter()
+
+const authStore = useAuthStore();
+
+// Create an instance of axios to use in this module.
+// See defaults set in /src/main.ts
+const http_client = axios.create();
+
+// Set the AUTH token for any request
+http_client.interceptors.request.use(function (config) {
+  if (authStore.isAuthenticated) {
+    const token = authStore.getToken;
+    console.log('http_client.interceptors.request.use()', token);
+    config.headers.Authorization = token ? `Token ${token}` : '';
+  }
+  return config;
+});
+
+// Demonstrates how to use environment variables in Vue components.
+console.log('import.meta.env.VITE_BASE_URL=' + import.meta.env.VITE_BASE_URL)
+
+/**
+* Makes a GET request to the current user endpoint and sets the user value if the response status is within the 200-299 range.
+* If the response status is outside the 200-299 range, an error message is logged to the console.
+* If there is a network error or the request is rejected, an error message is logged to the console.
+* Note: axios automatically rejects the promise if the status code is outside the 200-299 range, so the else branch in this example might not be necessary depending on your axios configuration.
+*/
+onMounted(async () => {
+  try {
+    const response = await http_client.get('/api/users/current_user/');
+    if (response.status >= 200 && response.status < 300) {
+      user = reactive(Object.assign(user, response.data));
+      /**
+       * TODO:
+       * Basing the is_authenticated value on the response status
+       * code is not ideal. The response status code is 200 even
+       * if the user is not authenticated. The response data
+       * contains the user object if the user is authenticated.
+       * It also leads to a sitch where some parts of the UI are
+       * rendered before the user object is set; so the global
+       * nav is rendered with the auth links but the profie
+       * dropdown has the guest links.
+       *
+       **/
+      user.is_authenticated = true;
+      console.log('onMounted() user=', user);
+    } else {
+      // console.error(`Request failed with status code ${response.status}`);
+      user = reactive(Object.assign(user, guestUser.value));
+    }
+  } catch (error) {
+    // console.error('Request failed', error);
+    user = reactive(Object.assign(user, guestUser.value));
+  }
+});
+
+let user = reactive({
+  is_admin: false,
+  is_staff: false,
+  is_authenticated: false,
+  name: '',
+  username: '',
+  email: '',
+  imageUrl: defaultAvatar,
+});
+
+let guestUser = ref({
+  is_admin: false,
+  is_staff: false,
+  is_authenticated: false,
+  name: 'Heart Cat',
+  username: 'heartcat@animalfoodbank.org',
+  email: '',
+  imageUrl: defaultAvatar,
+});
+
+// Get routes by name
+const dashboardRoute = router.resolve('dashboard');
+const aboutRoute = router.resolve('/about');
+const requestsRoute = router.resolve('/requests');
+
+const navigation = [
+  {
+    name: "Dashboard",
+    href: dashboardRoute.path,
+  },
+  {
+    name: "About",
+    href: aboutRoute.path,
+  },
+  {
+    name: "Make a request",
+    href: requestsRoute.path,
+  },
+]
+
+const profileNav = [
+  {
+    name: 'Your Profile',
+    href: '/profile',
+    is_authenticated: true,
+  },
+  {
+    name: 'Your dashboard',
+    href: '/dashboard'
+  },
+  {
+    name: 'Sign out',
+    href: '/logout'
+  },
+]
+
+const guestNavigation = [
+  { name: 'Sign in', href: '/login' },
+];
+
+/**
+ * The navigationItems computed property will be re-computed whenever
+ * the user.is_authenticated reactive property changes. In your case,
+ * navigationItems will return either profileNav or guestNavigation
+ * based on the user.is_authenticated property. So, whenever the
+ * authentication status of the user changes (i.e., user.is_authenticated
+ * changes), Vue.js will automatically re-compute the navigationItems.
+**/
+const navigationItems = computed(() => {
+  console.log("navigationItems computed(): user.is_authenticated=", user.is_authenticated)
+  return user.is_authenticated ? profileNav : guestNavigation;
+});
+
+const footerNavigation = {
+  main: [
+    { name: 'Blog', href: '#' },
+    { name: 'About Us', href: '/about' },
+    { name: 'Pet Food Recovery Program', href: 'https://animalfoodbank.org/afb-national-pet-food-recovery-program/' },
+    { name: 'Get Help', href: 'https://animalfoodbank.org/get-help/' },
+    { name: 'AFB Pet Club', href: 'https://afbpetclub.org' },
+    { name: 'Sponsorship', href: 'https://animalfoodbank.org/sponsorship/' },
+  ],
+  social: [
+    {
+      name: 'Facebook',
+      href: 'https://www.facebook.com/animalfoodbankcanada',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+            h('path', {
+              'fill-rule': 'evenodd',
+              d: 'M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z',
+              'clip-rule': 'evenodd',
+            }),
+          ]),
+      }),
+    },
+    {
+      name: 'Instagram',
+      href: 'https://www.instagram.com/animalfoodbank/',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+            h('path', {
+              'fill-rule': 'evenodd',
+              d: 'M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z',
+              'clip-rule': 'evenodd',
+            }),
+          ]),
+      }),
+    },
+    {
+      name: 'GitHub',
+      href: 'https://github.com/AnimalFoodBank',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+            h('path', {
+              'fill-rule': 'evenodd',
+              d: 'M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z',
+              'clip-rule': 'evenodd',
+            }),
+          ]),
+      }),
+    },
+  ],
+}
+</script>
+
 <template>
   <!--
     This example requires updating your template:
@@ -160,182 +356,6 @@
 </footer>a
 </div>
 </template>
-
-<!-- NOTE: When setup is missing, the router/index.ts shows a warning that ApplicationPage isn't a module -->
-<script setup lang="ts">
-import defaultAvatar from '@/assets/img/Cat-Heart-680x800-1.png';
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline';
-import axios from 'axios';
-import { computed, defineComponent, h, onMounted, reactive, ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { useAuthStore } from '../stores/auth';
-
-// See: https://router.vuejs.org/api/interfaces/Router.html
-const router = useRouter()
-
-const authStore = useAuthStore();
-
-// Create an instance of axios to use in this module.
-// See defaults set in /src/main.ts
-const http_client = axios.create();
-
-// Set the AUTH token for any request
-http_client.interceptors.request.use(function (config) {
-  if (authStore.isAuthenticated) {
-    const token = authStore.getToken;
-    console.log('http_client.interceptors.request.use()', token);
-    config.headers.Authorization = token ? `Token ${token}` : '';
-  }
-  return config;
-});
-
-// Demonstrates how to use environment variables in Vue components.
-console.log('import.meta.env.VITE_BASE_URL=' + import.meta.env.VITE_BASE_URL)
-
-/**
-* Makes a GET request to the current user endpoint and sets the user value if the response status is within the 200-299 range.
-* If the response status is outside the 200-299 range, an error message is logged to the console.
-* If there is a network error or the request is rejected, an error message is logged to the console.
-* Note: axios automatically rejects the promise if the status code is outside the 200-299 range, so the else branch in this example might not be necessary depending on your axios configuration.
-*/
-onMounted(async () => {
-  try {
-    const response = await http_client.get('/api/users/current_user/');
-    if (response.status >= 200 && response.status < 300) {
-      user = reactive(Object.assign(user, response.data));
-      user.is_authenticated = true;
-      console.log('onMounted() user=', user);
-    } else {
-      // console.error(`Request failed with status code ${response.status}`);
-      user = reactive(Object.assign(user, guestUser.value));
-    }
-  } catch (error) {
-    // console.error('Request failed', error);
-    user = reactive(Object.assign(user, guestUser.value));
-  }
-});
-
-let user = reactive({
-  is_admin: false,
-  is_staff: false,
-  is_authenticated: false,
-  name: '',
-  username: '',
-  email: '',
-  imageUrl: defaultAvatar,
-});
-
-let guestUser = ref({
-  is_admin: false,
-  is_staff: false,
-  is_authenticated: false,
-  name: 'Heart Cat',
-  username: 'heartcat@animalfoodbank.org',
-  email: '',
-  imageUrl: defaultAvatar,
-});
-
-// Get routes by name
-const dashboardRoute = router.resolve('dashboard');
-const aboutRoute = router.resolve('/about');
-const requestsRoute = router.resolve('/requests');
-
-const navigation = [
-{
-  name: "Dashboard",
-  href: dashboardRoute.path,
-},
-{
-  name: "About",
-  href: aboutRoute.path,
-},
-{
-  name: "Make a request",
-  href: requestsRoute.path,
-},
-]
-
-const profileNav = [
-{
-  name: 'Your Profile',
-  href: '/profile',
-  is_authenticated: true,
-},
-{
-  name: 'Your dashboard',
-  href: '/dashboard'
-},
-{
-  name: 'Sign out',
-  href: '/logout'
-},
-]
-
-const guestNavigation = [
-{ name: 'Sign in', href: '/login' },
-];
-
-const navigationItems = computed(() => {
-  console.log(user.is_authenticated)
-  return user.is_authenticated ? profileNav : guestNavigation;
-});
-
-const footerNavigation = {
-  main: [
-  { name: 'Blog', href: '#' },
-  { name: 'About Us', href: '/about' },
-  { name: 'Pet Food Recovery Program', href: 'https://animalfoodbank.org/afb-national-pet-food-recovery-program/' },
-  { name: 'Get Help', href: 'https://animalfoodbank.org/get-help/' },
-  { name: 'AFB Pet Club', href: 'https://afbpetclub.org' },
-  { name: 'Sponsorship', href: 'https://animalfoodbank.org/sponsorship/' },
-  ],
-  social: [
-  {
-    name: 'Facebook',
-    href: 'https://www.facebook.com/animalfoodbankcanada',
-    icon: defineComponent({
-      render: () =>
-      h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-      h('path', {
-        'fill-rule': 'evenodd',
-        d: 'M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z',
-        'clip-rule': 'evenodd',
-      }),
-      ]),
-    }),
-  },
-  {
-    name: 'Instagram',
-    href: 'https://www.instagram.com/animalfoodbank/',
-    icon: defineComponent({
-      render: () =>
-      h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-      h('path', {
-        'fill-rule': 'evenodd',
-        d: 'M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z',
-        'clip-rule': 'evenodd',
-      }),
-      ]),
-    }),
-  },
-  {
-    name: 'GitHub',
-    href: 'https://github.com/AnimalFoodBank',
-    icon: defineComponent({
-      render: () =>
-      h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-      h('path', {
-        'fill-rule': 'evenodd',
-        d: 'M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z',
-        'clip-rule': 'evenodd',
-      }),
-      ]),
-    }),
-  },
-  ],
-}
-</script>
 
 
 
