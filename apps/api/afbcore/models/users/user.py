37060724f186e22906f885a922eb37d9d580d2f4
delano <delano@cpan.org>
2023-11-24 15:59:00 -0800

    Add custom UserManager to User model
    Signed-off-by: delano <delano@cpan.org>

--- apps/api/afbcore/models/users/user.py
+++ apps/api/afbcore/models/users/user.py
@@ -1,12 +1,3 @@
-from django.contrib.auth.models import UserManager as DefaultUserManager
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _
-from model_utils.models import (
-    TimeStampedModel,
-    UUIDModel,
-)
-
 """
     AFB Core API User Model
 
@@ -20,6 +11,51 @@ from model_utils.models import (
     ever delete Users using the user.delete() method.
 
 """
+
+from django.contrib.auth.models import UserManager as DefaultUserManager
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import (
+    TimeStampedModel,
+    UUIDModel,
+)
+
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(UUIDModel, TimeStampedModel, AbstractUser):
@@ -43,6 +79,9 @@ class User(UUIDModel, TimeStampedModel, AbstractUser):
     - updated: DateTimeField, auto_now=True
     """
 
+    # Use custom manager.
+    objects = UserManager()
+
     USERNAME_FIELD = "email"
 
     # From the docs:
@@ -52,7 +91,6 @@ class User(UUIDModel, TimeStampedModel, AbstractUser):
     # fields will always be prompted for.
     REQUIRED_FIELDS = [
         "name",
-        "terms_agreement",
     ]
 
     email = models.EmailField(_("email address"), unique=True)
