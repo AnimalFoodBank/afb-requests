4dcd65e553118f8df920ff512e289b951345d622
delano <delano@cpan.org>
2023-12-03 09:10:42 -0800

    Clean up urlpatterns, naming
    - Updated router registration for UserViewSet to include a basename.
    - Assigned names to existing admin and token auth paths for better URL name management.
    
    This work is related to debugging the issues around `HyperlinkedIdentityField` coming from the register serializer.
    
    Signed-off-by: delano <delano@cpan.org>

--- apps/api/afb/urls/__init__.py
+++ apps/api/afb/urls/__init__.py
@@ -13,15 +13,36 @@ from afbcore.views import users, public
 from afbcore.contrib.api_router import APIRouter
 from .extra_api_views import EXTRA_API_VIEWS
 
+"""
+
+In settings:
+REST_FRAMEWORK = {
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+    'ALLOWED_VERSIONS': ['v1', 'v2'],
+    'VERSION_PARAM': 'version'
+}
+
+Then in here:
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('api/<str:version>/users/', include([
+        path('', views.UserList.as_view(), name='user-list'),
+        path('<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    ])),
+]
+
+"""
 router = APIRouter(singleViews=EXTRA_API_VIEWS)
-router.register(r"users", users.UserViewSet)
+router.register(r"users", users.UserViewSet, basename="user")
 
 
 # TODO: Prefix with /version
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls, name="admin"),
     path("api/", include(router.urls)),
-    path("auth-token/", obtain_auth_token),
+    path("auth-token/", obtain_auth_token, name="api_token_auth"),
     # Add DRF API registration endpoints. We need these for the browsable API.
     path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),
     # Add drf_registration endpoints. These are used for login, registration,
