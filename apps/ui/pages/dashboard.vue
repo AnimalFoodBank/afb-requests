815d09eb37a822652ae1799a443407dd74c867b7
delano <delano@cpan.org>
2024-02-02 16:34:48 -0800

    [#57] Add new UI components for authentication
    - Added `GithubIcon`, `GithubLogin`, `Login`, `LoginForm`, `Logo`, `Navbar`, `NextAuthIcon`, and `dashboard` as new Vue components, enhancing the UI with authentication functionalities.
    - Modified `login.vue` to introduce improved layout, including the `unauthenticatedOnly` policy and commented out unnecessary debugger statement.
    - Removed unnecessary props and slots from `UAuthForm` in `login.vue` to streamline the code.
    - Adjusted some components to follow the established project styling and conventions.
    
    NOTE: These are based on the Nuxt Auth Crash Course:
    https://github.com/CodyBontecou/Nuxt-Auth-Tutorial

---
+++ apps/ui/pages/dashboard.vue
@@ -0,0 +1,4 @@
+
+<template>
+  <h1 class="text-3xl font-bold">Hi! I'm protected.</h1>
+</template>
--- apps/ui/pages/login.vue
+++ apps/ui/pages/login.vue
@@ -2,7 +2,11 @@
 const config = useRuntimeConfig();
 
 definePageMeta({
-  layout: 'auth'
+  layout: 'auth',
+  auth: {
+    unauthenticatedOnly: true,
+    // navigateAuthenticatedTo: '/protected',
+  },
 })
 
 useSeoMeta({
@@ -68,7 +72,7 @@ async function onSubmit (formData: any) {
   })
 
   if (error) {
-    debugger
+    // debugger
     console.error('An error occurred:', error)
     console.log(error)
     return
@@ -84,28 +88,34 @@ async function onSubmit (formData: any) {
 <!-- eslint-disable vue/singleline-html-element-content-newline -->
 <template>
   <UCard class="max-w-sm w-full bg-white/75 dark:bg-white/5 backdrop-blur">
+
     <UAuthForm
-      :fields="fields"
-      :validate="validate"
-      :providers="providers"
-      title="Welcome back"
-      align="top"
-      icon="i-heroicons-lock-closed"
-      :ui="{ base: 'text-center', footer: 'text-center' }"
-      :submit-button="{ trailingIcon: 'i-heroicons-arrow-right-20-solid' }"
-      @submit="onSubmit"
-    >
-      <template #description>
-        Don't have an account? <NuxtLink to="/signup" class="text-primary font-medium">Sign up</NuxtLink>.
-      </template>
+        :fields="fields"
+        :validate="validate"
+        :providers="providers"
+        title="Welcome back"
+        align="top"
+        icon="i-heroicons-lock-closed"
+        :ui="{ base: 'text-center', footer: 'text-center' }"
+        :submit-button="{ trailingIcon: 'i-heroicons-arrow-right-20-solid' }"
+        @submit="onSubmit"
+      >
+        <template #description>
+          Don't have an account? <NuxtLink to="/signup" class="text-primary font-medium">Sign up</NuxtLink>.
+        </template>
 
-      <template #password-hint>
-        <NuxtLink to="/" class="text-primary font-medium">Forgot password?</NuxtLink>
-      </template>
+        <template #password-hint>
+          <NuxtLink to="/" class="text-primary font-medium">Forgot password?</NuxtLink>
+        </template>
 
-      <template #footer>
-        By signing in, you agree to our <NuxtLink to="/" class="text-primary font-medium">Terms of Service</NuxtLink>.
-      </template>
+        <template #footer>
+          By signing in, you agree to our <NuxtLink to="/" class="text-primary font-medium">Terms of Service</NuxtLink>.
+        </template>
     </UAuthForm>
+
+
+    <!-- <Login /> -->
+
   </UCard>
+
 </template>
