b29cc9b91cf6cb1a76332f8ba012fa49e00594a2
delano <delano@cpan.org>
2024-02-07 15:06:50 -0800

    [#57] Add LoginPasswordlessForm component

---
+++ apps/ui/components/LoginPasswordlessForm.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { FormError, FormSubmitEvent } from '#ui/types';
+
+const state = reactive({
+  email: undefined,
+})
+
+const validate = (state: any): FormError[] => {
+  const errors = []
+  if (!state.email) errors.push({ path: 'email', message: 'Required' })
+  return errors
+}
+
+async function onSubmit (event: FormSubmitEvent<any>) {
+  // Do something with data
+  console.log(event.data)
+}
+</script>
+
+<template>
+  <UForm :validate="validate" :state="state" class="space-y-4" @submit="onSubmit">
+    <UFormGroup label="Email" name="email">
+      <UInput v-model="state.email" />
+    </UFormGroup>
+
+    <UButton type="submit">
+      Submit
+    </UButton>
+  </UForm>
+</template>
