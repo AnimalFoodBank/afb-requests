f2900fe7654dccdb249d1bcf46d00e1ac2b39e22
delano <delano@cpan.org>
2024-02-12 22:40:14 -0800

    [#57] Update Header component with new links and logo

--- apps/ui/components/Header.vue
+++ apps/ui/components/Header.vue
@@ -1,29 +1,32 @@
 <script setup lang="ts">
-import type { NavItem } from '@nuxt/content/dist/runtime/types'
+import type { NavItem } from '@nuxt/content/dist/runtime/types';
 
 const navigation = inject<Ref<NavItem[]>>('navigation', ref([]))
 
 const links = [{
-  label: 'Docs',
-  to: '/docs'
+  label: 'Request',
+  to: '/request'
 }, {
-  label: 'Pricing',
-  to: '/pricing'
+  label: 'Dashboard',
+  to: '/dashboard'
 }, {
-  label: 'Blog',
-  to: '/blog'
+  label: 'About Us',
+  to: '/about'
 }]
 </script>
 
 <template>
   <UHeader :links="links">
     <template #logo>
-      Nuxt UI Pro <UBadge label="SaaS" variant="subtle" class="mb-0.5" />
+      <!-- AFB <UBadge label="Registered Non-Profit" variant="subtle" class="mb-0.5" /> -->
+      <NuxtLink to="/">
+        <nuxt-img src="/img/afb_logo_horizontal_colour.png" alt="Animal Food Bank Logo" class="max-w-32 min-16"/>
+      </NuxtLink>
     </template>
 
     <template #right>
-      <UButton label="Log in" color="gray" to="/login" />
-      <UButton label="Sign up" icon="i-heroicons-arrow-right-20-solid" trailing color="black" to="/signup" class="hidden lg:flex" />
+      <!-- <UButton label="Sign in" color="gray" to="/login" /> -->
+      <UButton label="Sign in" icon="i-heroicons-arrow-right-20-solid" trailing color="black" to="/login" class="hidden lg:flex" />
     </template>
 
     <template #panel>
