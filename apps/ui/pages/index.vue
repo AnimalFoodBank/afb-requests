c8cdce2c0a929032aaa32c535c184ee0a86832ca
delano <delano@cpan.org>
2024-02-13 17:39:28 -0800

    [#66] Remove unused content and refactor styles
    - Cleared outdated `sections`, `features`, `testimonials`, and `cta` from the `0.index.yml` content file.
    - Simplified `index.vue` by removing placeholders for the deleted content sections.
    - Commented out the old `.landing-grid` styling in prep for re-styling efforts.

--- apps/ui/pages/index.vue
+++ apps/ui/pages/index.vue
@@ -13,7 +13,6 @@ useSeoMeta({
 })
 
 definePageMeta({
-  // colorMode: 'dark',
 })
 
 defineOgImage({
@@ -29,53 +28,22 @@ defineOgImage({
       <div class="absolute inset-0 landing-grid z-[-1] [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" />
     </ULandingHero>
 
-    <ULandingSection class="!pt-0">
-      <Placeholder />
-    </ULandingSection>
-
-    <ULandingSection
-      v-for="(section, index) in page.sections"
-      :key="index"
-      :title="section.title"
-      :description="section.description"
-      :align="section.align"
-      :features="section.features"
-    >
-      <Placeholder />
-    </ULandingSection>
-
-    <ULandingSection :title="page.features.title" :description="page.features.description">
-      <UPageGrid>
-        <ULandingCard v-for="(item, index) in page.features.items" :key="index" v-bind="item" />
-      </UPageGrid>
-    </ULandingSection>
-
-    <ULandingSection :headline="page.testimonials.headline" :title="page.testimonials.title" :description="page.testimonials.description">
-      <UPageColumns class="xl:columns-4">
-        <div v-for="(testimonial, index) in page.testimonials.items" :key="index" class="break-inside-avoid">
-          <ULandingTestimonial v-bind="testimonial" class="bg-gray-100/50 dark:bg-gray-800/50" />
-        </div>
-      </UPageColumns>
-    </ULandingSection>
-
-    <ULandingSection>
-      <ULandingCTA v-bind="page.cta" class="bg-gray-100/50 dark:bg-gray-800/50" />
-    </ULandingSection>
   </div>
 </template>
 
 <style scoped>
-.landing-grid {
-  background-size: 100px 100px;
-  background-image:
-    linear-gradient(to right, rgb(var(--color-gray-200)) 1px, transparent 1px),
-    linear-gradient(to bottom, rgb(var(--color-gray-200)) 1px, transparent 1px);
-}
-.dark {
-  .landing-grid {
-    background-image:
-      linear-gradient(to right, rgb(var(--color-gray-800)) 1px, transparent 1px),
-      linear-gradient(to bottom, rgb(var(--color-gray-800)) 1px, transparent 1px);
-  }
-}
+/**
+*   background-size: 100px 100px;
+*   background-image:
+*     linear-gradient(to right, rgb(var(--color-gray-200)) 1px, transparent 1px),
+*     linear-gradient(to bottom, rgb(var(--color-gray-200)) 1px, transparent 1px);
+* }
+* .dark {
+*   .landing-grid {
+*     background-image:
+*       linear-gradient(to right, rgb(var(--color-gray-800)) 1px, transparent 1px),
+*       linear-gradient(to bottom, rgb(var(--color-gray-800)) 1px, transparent 1px);
+*   }
+* }
+**/
 </style>
