71aa2a098b64c80b1f67b4e0affb6b5591eddc02
delano <delano@cpan.org>
2024-03-05 16:08:26 -0800

    [#20240228] Fix for missing dashboard nav

--- apps/ui/pages/requests/index.vue
+++ apps/ui/pages/requests/index.vue
@@ -15,17 +15,30 @@ const q = ref('')
 </script>
 
 <template>
-  <UDashboardPanelContent class="pb-24">
-    <UDashboardSection title="Make a new request" description="Send a food request to your local AFB branch." orientation="horizontal" :ui="{ container: 'lg:sticky top-2' }">
-      <template #links>
-      </template>
+  <UDashboardPage>
+    <UDashboardPanel>
+      <UDashboardNavbar title="Dashboard">
 
-      <UCard :ui="{ header: { padding: 'p-4 sm:px-6' }, body: { padding: '' } }" class="min-w-0">
-        <template #header>
-          <UInput v-model="q" icon="i-heroicons-magnifying-glass" placeholder="Search members" autofocus />
-        </template>
-      </UCard>
-    </UDashboardSection>
+      </UDashboardNavbar>
 
-  </UDashboardPanelContent>
+      <UDashboardToolbar>
+      </UDashboardToolbar>
+
+      <UDashboardPanelContent class="pb-24">
+        <UDashboardSection title="Make a new request" description="Send a food request to your local AFB branch." orientation="horizontal" :ui="{ container: 'lg:sticky top-2' }">
+          <template #links>
+            <UButton label="New Request" color="black" @click="isInviteModalOpen = true" />
+          </template>
+
+          <UCard :ui="{ header: { padding: 'p-4 sm:px-6' }, body: { padding: '' } }" class="min-w-0">
+            <template #header>
+              <UInput v-model="q" icon="i-heroicons-magnifying-glass" placeholder="Search members" autofocus />
+            </template>
+          </UCard>
+        </UDashboardSection>
+
+      </UDashboardPanelContent>
+    </UDashboardPanel>
+  </UDashboardPage>
+
 </template>
