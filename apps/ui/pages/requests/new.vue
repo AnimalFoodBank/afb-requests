cc0ead9ac9f86162d461d616f381b42084482454
delano <delano@cpan.org>
2024-03-08 19:22:18 -0800

    [#28] Add FoodRequestForm component

--- apps/ui/pages/requests/new.vue
+++ apps/ui/pages/requests/new.vue
@@ -9,6 +9,7 @@ definePageMeta({
   // colorMode: 'dark',
 })
 
+const isDeleteAccountModalOpen = ref(false)
 const state = reactive({
   name: 'Poo Bear',
   email: 'poop@solutious.com',
@@ -41,97 +42,10 @@ const state = reactive({
 
         <UDivider class="mb-4" />
 
-        <UForm :state="state" :validate="validate" :validate-on="['submit']" @submit="onSubmit">
-          <UDashboardSection title="Profile" description="This information will be displayed publicly so be careful what you share.">
-            <template #links>
-              <UButton type="submit" label="Save changes" color="black" />
-            </template>
 
-            <UFormGroup
-              name="name"
-              label="Name"
-              description="Will appear on receipts, invoices, and other communication."
-              required
-              class="grid grid-cols-2 gap-2 items-center"
-              :ui="{ container: '' }"
-            >
-              <UInput v-model="state.name" autocomplete="off" icon="i-heroicons-user" size="md" />
-            </UFormGroup>
 
-            <UFormGroup
-              name="email"
-              label="Email"
-              description="Used to sign in, for email receipts and product updates."
-              required
-              class="grid grid-cols-2 gap-2"
-              :ui="{ container: '' }"
-            >
-              <UInput v-model="state.email" type="email" autocomplete="off" icon="i-heroicons-envelope" size="md" />
-            </UFormGroup>
 
-            <UFormGroup
-              name="username"
-              label="Username"
-              description="Your unique username for logging in and your profile URL."
-              required
-              class="grid grid-cols-2 gap-2"
-              :ui="{ container: '' }"
-            >
-              <UInput v-model="state.username" type="username" autocomplete="off" size="md" input-class="ps-[77px]">
-                <template #leading>
-                  <span class="text-gray-500 dark:text-gray-400 text-sm">nuxt.com/</span>
-                </template>
-              </UInput>
-            </UFormGroup>
 
-            <UFormGroup name="avatar" label="Avatar" class="grid grid-cols-2 gap-2" help="JPG, GIF or PNG. 1MB Max." :ui="{ container: 'flex flex-wrap items-center gap-3', help: 'mt-0' }">
-              <UAvatar :src="state.avatar" :alt="state.name" size="lg" />
-
-              <UButton label="Choose" color="white" size="md" @click="onFileClick" />
-
-              <UInput ref="fileRef" type="file" class="hidden" accept=".jpg, .jpeg, .png, .gif" @change="onFileChange" />
-            </UFormGroup>
-
-            <UFormGroup
-              name="bio"
-              label="Bio"
-              description="Brief description for your profile. URLs are hyperlinked."
-              class="grid grid-cols-2 gap-2"
-              :ui="{ container: '' }"
-            >
-              <UTextarea v-model="state.bio" :rows="5" autoresize size="md" />
-            </UFormGroup>
-
-            <UFormGroup
-              name="password"
-              label="Password"
-              description="Confirm your current password before setting a new one."
-              class="grid grid-cols-2 gap-2"
-              :ui="{ container: '' }"
-            >
-              <UInput id="password" v-model="state.password_current" type="password" placeholder="Current password" size="md" />
-              <UInput
-                id="password_new"
-                v-model="state.password_new"
-                type="password"
-                placeholder="New password"
-                size="md"
-                class="mt-2"
-              />
-            </UFormGroup>
-          </UDashboardSection>
-        </UForm>
-
-        <UDivider class="mb-4" />
-
-        <UDashboardSection title="Account" description="No longer want to use our service? You can delete your account here. This action is not reversible. All information related to this account will be deleted permanently.">
-          <div>
-            <UButton color="red" label="Delete account" size="md" @click="isDeleteAccountModalOpen = true" />
-          </div>
-        </UDashboardSection>
-
-        <!-- ~/components/settings/DeleteAccountModal.vue -->
-        <SettingsDeleteAccountModal v-model="isDeleteAccountModalOpen" />
       </UDashboardPanelContent>
 
 
