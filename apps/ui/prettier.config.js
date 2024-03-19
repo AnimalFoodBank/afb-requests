e9c640e40b4ef2a4b1d179b77607810835c6a12e
delano <delano@cpan.org>
2024-03-19 08:39:19 -0700

    [#80] Add prettier configuration file

---
+++ apps/ui/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  charset: "utf-8",
+  insert_final_newline: true,
+  end_of_line: "lf",
+  indent_style: "space",
+  indent_size: 2,
+  max_line_length: 80,
+  useTabs: false,
+  prettier: 'prettier --write "apps/ui/**/*.{ts,tsx,vue}"',
+};
