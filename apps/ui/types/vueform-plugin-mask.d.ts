7ba531903aa3fd9e2ec976786425006cbf0c33f6
delano <delano@cpan.org>
2024-03-21 07:02:37 -0700

    [#38] Add declaration file for Vueform Mask plugin

---
+++ apps/ui/types/vueform-plugin-mask.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @file vueform-plugin-mask.d.ts
+ * @description This file is a declaration file for the Vueform Mask plugin.
+ * @module Types PluginMask
+ * @see {@link https://github.com/vueform/plugin-mask}
+ *
+ * @example Resolves the inline warning when importing the plugin in the
+ * vueform.config.ts file:
+ *
+ *    import PluginMask from '@vueform/plugin-mask'
+ *
+ */
+declare module '@vueform/plugin-mask';
