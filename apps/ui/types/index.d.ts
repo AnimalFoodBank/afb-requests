ec92a5689664c68bde9202106a135f720111b8f6
delano <delano@cpan.org>
2024-03-21 12:46:34 -0700

    [#38] Add FoodDeliveryFormState interface

--- apps/ui/types/index.d.ts
+++ apps/ui/types/index.d.ts
@@ -12,3 +12,39 @@ export interface BlogPost extends ParsedContent {
     avatar?: Avatar
   } & Link)[]
 }
+
+export interface FoodDeliveryFormState {
+  // Define your state properties here
+  delivery_address: {
+      branch_location: string;
+      location: {
+        address_line1: string;
+        city: string;
+        divisions_level1: string;
+        postcode: string;
+        country: string;
+      };
+      building_type: string;
+    },
+    delivery_contact:{
+      contact_number: string;
+      contact_name: string;
+      preferred_method: string;
+    },
+    your_pets: {
+      pet_name: string;
+      pet_breed: string;
+      pet_age: string;
+      pet_weight: string;
+    },
+    safe_drop: {
+      safe_drop_policy: string;
+      safe_drop: boolean;
+      safe_drop_instructions: string;
+    },
+    confirmation: {
+      confirm_correct: boolean;
+      accept_terms: boolean;
+    };
+  // Add more properties as needed
+}
