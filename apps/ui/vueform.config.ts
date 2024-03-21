f8230c6b9f6b6ed9fda4a8d9a624c1f62bfa04c8
delano <delano@cpan.org>
2024-03-20 21:32:28 -0700

    [#38] Add @vueform/plugin-mask dependency

--- apps/ui/vueform.config.ts
+++ apps/ui/vueform.config.ts
@@ -5,13 +5,17 @@ import vueform from '@vueform/vueform/themes/vueform'
 import tailwind from '@vueform/vueform/dist/tailwind'
 import { defineConfig } from '@vueform/vueform'
 import builder from '@vueform/builder/plugin'
+import PluginMask from '@vueform/plugin-mask'
 
 export default defineConfig({
   theme: tailwind,
   locales: { en },
   locale: 'en',
   apiKey: 'sppy-spc3-valx-p0vf-sej6',
+  endpoints: {
+  },
   plugins: [
     builder,
+    PluginMask,
   ],
 })
