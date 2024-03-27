47d8f47e224a8d2d019b7e4c669c32b5e96e7382
delano <delano@cpan.org>
2024-03-27 11:45:46 -0700

    [#80] Update login page to comment out sign up link

--- apps/ui/pages/login/magic.vue
+++ apps/ui/pages/login/magic.vue
@@ -131,7 +131,7 @@ async function onSubmit(event: FormSubmitEvent<{ email: string }>) {
     >
       <div class="w-full max-w-sm space-y-6">
         <h2 class="text-2xl text-gray-900 dark:text-white font-bold text-center">Sign in to AFB Requests</h2>
-        <p class="text-gray-500 dark:text-gray-400 mt-1 text-center">Don't have an account? <NuxtLink to="/login" class="text-primary font-medium">Sign up</NuxtLink>.</p>
+        <!-- <p class="text-gray-500 dark:text-gray-400 mt-1 text-center">Don't have an account? <NuxtLink to="/login" class="text-primary font-medium">Sign up</NuxtLink>.</p> -->
 
         <UInput v-model="email" name="email" type="hidden" label="Email" required />
         <UInput v-model="code" name="code" type="hidden" label="Code" required />
