1693d1388255bc0c93ce65e83bb426ead98b4b3f
delano <delano@cpan.org>
2024-04-25 06:39:24 -0700

    [#155] Reduce session refresh frequency to improve performance
    This change increases the periodic session refresh interval from 1 minute to 5 minutes to lower server load.

--- apps/ui/nuxt.config.ts
+++ apps/ui/nuxt.config.ts
@@ -129,8 +129,10 @@ export default defineNuxtConfig({
       // Whether to refresh the session every time the browser window is refocused.
       enableRefreshOnWindowFocus: true,
 
-      // Whether to refresh the session every `X` milliseconds. Set this to `false` to turn it off. The session will only be refreshed if a session already exists.
-      enableRefreshPeriodically: 60000
+      // Whether to refresh the session every `X` milliseconds. Set
+      // this to `false` to turn it off. The session will only be
+      // refreshed if a session already exists.
+      enableRefreshPeriodically: 60000 * 5, // 5 minutes
     },
   },
 
