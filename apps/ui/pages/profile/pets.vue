749bdfeed48d97648c9368d61e580d9340976273
delano <delano@cpan.org>
2024-04-30 19:00:59 -0700

    [#134] Improve profile pets page wording and structure
    Clarify pet application follow up text. Add a new section heading "Pet
    History" and display placeholder text that no changes have been made
    yet. Minor formatting tweaks.

--- apps/ui/pages/profile/pets.vue
+++ apps/ui/pages/profile/pets.vue
@@ -18,21 +18,26 @@ const cta = false
       </template>
     </UDashboardSection>
 
-    <!-- <UDashboardCard class="mx-9 mb-1 max-w-prose">
-      <UDashboardSection class="mb-2 text-md italic">
-
-      </UDashboardSection>
-    </UDashboardCard> -->
-
     <UDashboardSection>
       <PetsForm :cta="false" :state="state" />
     </UDashboardSection>
 
-    <UDivider class="mb-4" />
-
-    <UDashboardSection class="italic">
-      Please note that we review pet details and may follow-up for verification.
+    <UDashboardSection class="mb-8 text-md italic">
+      Please note we may follow-up with you if we need more information.
     </UDashboardSection>
+
+    <UDashboardSection class="">
+      <p class="text-xl font-bold text-gray-900 dark:text-gray-50">Pet History</p>
+    </UDashboardSection>
+
+    <UDashboardCard class="mx-9 mb-1 max-w-prose">
+      <UDashboardSection class="mb-2 text-md text-base text-gray-500">
+      No changes to display yet.
+      </UDashboardSection>
+    </UDashboardCard>
+
+    <!-- <UDivider class="mb-4" />
+ -->
 
   </UDashboardPanelContent>
 </template>
