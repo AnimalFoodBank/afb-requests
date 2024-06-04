2e33ad805edcee15f0d18d6c52717b772cb8e333
Delano <1206+delano@users.noreply.github.com>
2024-06-04 15:06:24 -0700

    Update apps/api/afbcore/models/food_request.py
    Co-authored-by: codiumai-pr-agent-pro[bot] <151058649+codiumai-pr-agent-pro[bot]@users.noreply.github.com>
    Signed-off-by: Delano <1206+delano@users.noreply.github.com>

--- apps/api/afbcore/models/food_request.py
+++ apps/api/afbcore/models/food_request.py
@@ -97,7 +97,8 @@ class FoodRequest(BaseAbstractModel):
 
     # Request Received, Request Approved & in Queue, Request Denied, Volunteer Assigned, Request Ready For Volunteer Pickup, Delivery Scheduled, Out For Delivery, Delivered, Undeliverable
     request_status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default="received"
+        max_length=50, choices=STATUS_CHOICES, default="received"
+    )
     )
 
     # Free form comments from driver, client, volunteer, etc.
